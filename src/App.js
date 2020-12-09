72edda422a0d48dbfacc726ba066975aa3e463a0
Bruno Belo <brunohbelo@hotmail.com>
2020-12-09 19:00:07 -0300

    introduzido bug sonar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    
+    //sonar bug
+    let i = 0;
+    i = i++;
+    
+  }
+
   render() {
     return (
       <div className="App">
